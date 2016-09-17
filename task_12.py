8af70a7f4c663cf8190175063a556ec8685e59dd
jchan3 <joechan2014@gmail.com>
2016-09-17 22:17:19 +0000

    COMMIT

---
+++ task_12.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Docstring for Task 12"""
+
+from decimal import Decimal
+from fractions import Fraction
+
+
+INTVAL = 1
+
+FLOATVAL = 0.1
+
+DECVAL = Decimal('0.1')
+
+FRACVAL = Fraction(1, 10)
